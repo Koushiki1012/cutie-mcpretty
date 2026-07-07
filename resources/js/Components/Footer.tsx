478f248716d5b2fd4a8c5223c24aeff76379e052
Koushiki1012 <koushikichoudhary1012@gmail.com>
2026-07-07 07:26:04 +0530

    first commit

--- resources/js/Components/Footer.tsx
+++ resources/js/Components/Footer.tsx
@@ -8,14 +8,14 @@ export default function Footer({ user }: FooterProps) {
 
     return (
         <footer className='flex justify-center bg-gradient-to-l from-red-100 p-6' >
-            <p>@ Images courtesy of{' '}
+            <p>Images courtesy of{' '}
                 <a 
                     href="https://www.pexels.com" 
                     target="_blank" 
                     rel="noopener noreferrer"
                     className="font-semibold underline hover:text-gray-900"
                 >
-                    pexels.com
+                    Pexels
                 </a></p>
         </footer>
     );
