5e524b7d80281c9999b66cca4d8d505179200234
Koushiki1012 <koushikichoudhary1012@gmail.com>
2026-07-11 14:14:33 +0530

    Add image attribution

--- resources/js/Components/Footer.tsx
+++ resources/js/Components/Footer.tsx
@@ -9,14 +9,14 @@ export default function Footer({ user }: FooterProps) {
     return (
         <footer className='flex justify-center bg-gradient-to-l from-red-100 p-6' >
             <p>Images courtesy of{' '}
-                <a 
-                    href="https://www.pexels.com" 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                    className="font-semibold underline hover:text-gray-900"
-                >
-                    Pexels
-                </a></p>
+                <a href="https://www.pexels.com" target="_blank" rel="noopener noreferrer" className="mr-1 font-semibold underline hover:text-gray-900">
+                    Pexels 
+                </a>
+                 and
+                <a href='https://unsplash.com/' target='_blank' rel='noopener noreferrer' className="ml-1 font-semibold underline hover:text-gray-900">
+                    Unsplash 
+                </a>
+            </p>
         </footer>
     );
 }
